cd15bcd27d65fb527b72235ce295f1208379389e
Festus Ojo <fojo@apple.com>
2025-12-29 18:27:11 -0800

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Festus Ojo on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
